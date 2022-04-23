dba76fed3f147edcc7ecf6139a8ab913632c1dde
Jose Oviedo <guitaroviedo46@gmail.com>
2022-04-23 18:51:54 -0500

    layout

---
+++ resources/views/conferencista.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1 class="fw-titulo-1 pt-5">Perfil conferencistas</h1>
+    {{-- @foreach (($conferencistas as $item))
+        
+    @endforeach --}}
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
     <title>VI Semana Internacional de la Ingeniería UCC</title>
 </head>
 
@@ -50,10 +51,27 @@
                     </div>
                 </div>
             </nav>
-            <div class="text-center">
-                <p id="cuenta"></p>
+            <div class="fila-cuenta text-center">
+                <div class="d-inline-block me-3">
+                    <div class="cuenta" id="dias">00</div>
+                    <div class="titulo-cuenta">DÍAS</div>
+                </div>
+                <div class="d-inline-block me-3">
+                    <div class="cuenta" id="horas">00</div>
+                    <div class="titulo-cuenta">HORAS</div>
+                </div>
+                <div class="d-inline-block me-3">
+                    <div class="cuenta" id="minutos">00</div>
+                    <div class="titulo-cuenta">MINUTOS</div>
+                </div>
+                <div class="d-inline-block">
+                    <div class="cuenta" id="segundos">00</div>
+                    <div class="titulo-cuenta">SEGUNDOS</div>
+                </div>
+            </div>
+            <div class="text-center py-5">
                 <h1 class="display-4 fw-normal fw-white">VI Semana Internacional de la Ingeniería</h1>
-                <h3 class="fw-white">17 - 20 de mayo de 2022. Ibagué, Tolima, Colombia</h3>
+                <h3 class="fw-white">17 - 20 de mayo de 2022. Ibagué, Tolima, Colombia.</h3>
             </div>
         </div>
 
@@ -141,56 +159,27 @@
         </div>
     </main>
 
-    <footer class="container py-5">
+    <footer class="container-fluid pt-5 bg-azul-oscuro text-white">
         <div class="row">
-            <div class="col-12 col-md">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
-                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img"
-                    viewBox="0 0 24 24">
-                    <title>Product</title>
-                    <circle cx="12" cy="12" r="10" />
-                    <path
-                        d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
-                </svg>
-                <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
+            <div class="col-md"></div>
+            <div class="col-md"></div>
+            <div class="col-md py-3">
+                <h3 class="footer-heading">Síguenos</h3>
+                <a href="https://www.facebook.com/uccibaguespinal/" target="_blank" class="link-light me-2"><i class="fab fa-facebook-square fa-3x"></i></a>
+                <a href="https://twitter.com/uccibaguespinal" target="_blank" class="link-light me-2"><i class="fab fa-twitter-square fa-3x"></i></a>
+                <a href="https://www.youtube.com/channel/UCwLDxE0BoaD7tHL8ygbmNFw" target="_blank" class="link-light me-2"><i class="fab fa-youtube-square fa-3x"></i></a>
+                <a href="https://www.flickr.com/photos/ucooperativadecolombia" target="_blank" class="link-light me-2"><i class="fab fa-flickr fa-3x"></i></a>
+                <a href="https://www.instagram.com/ucc_ibaguespinal/" target="_blank" class="link-light me-2"><i class="fab fa-instagram fa-3x"></i></a>
             </div>
-            <div class="col-6 col-md">
-                <h5>Features</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="link-secondary" href="#">Cool stuff</a></li>
-                    <li><a class="link-secondary" href="#">Random feature</a></li>
-                    <li><a class="link-secondary" href="#">Team feature</a></li>
-                    <li><a class="link-secondary" href="#">Stuff for developers</a></li>
-                    <li><a class="link-secondary" href="#">Another one</a></li>
-                    <li><a class="link-secondary" href="#">Last time</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Resources</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="link-secondary" href="#">Resource name</a></li>
-                    <li><a class="link-secondary" href="#">Resource</a></li>
-                    <li><a class="link-secondary" href="#">Another resource</a></li>
-                    <li><a class="link-secondary" href="#">Final resource</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Resources</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="link-secondary" href="#">Business</a></li>
-                    <li><a class="link-secondary" href="#">Education</a></li>
-                    <li><a class="link-secondary" href="#">Government</a></li>
-                    <li><a class="link-secondary" href="#">Gaming</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>About</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="link-secondary" href="#">Team</a></li>
-                    <li><a class="link-secondary" href="#">Locations</a></li>
-                    <li><a class="link-secondary" href="#">Privacy</a></li>
-                    <li><a class="link-secondary" href="#">Terms</a></li>
-                </ul>
+        </div>
+        <div class="row text-center bg-azul-claro py-3">
+            <div class="container">
+                <p>Universidad Cooperativa de Colombia - Campus Ibagué, Espinal</p>
+                <p>Otra creación de 
+                    <a href="http://desarrollandoapps.net" target="_blank" class="text-decoration-none link-light">
+                        <img src="{{ asset('images/logo-da.svg') }}" alt="Logo desarrollandoApps" class="logo-da ms-3">
+                </a>
+                </p>
             </div>
         </div>
     </footer>
@@ -216,8 +205,10 @@
           var seconds = Math.floor((distance % (1000 * 60)) / 1000);
             
           // Output the result in an element with id="demo"
-          document.getElementById("cuenta").innerHTML = days + "d " + hours + "h "
-          + minutes + "m " + seconds + "s ";
+          document.getElementById("dias").innerHTML = days;
+          document.getElementById("horas").innerHTML = hours;
+          document.getElementById("minutos").innerHTML = minutes;
+          document.getElementById("segundos").innerHTML = seconds;
             
           // If the count down is over, write some text 
           if (distance < 0) {
